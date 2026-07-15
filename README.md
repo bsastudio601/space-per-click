726747f7b5f55cc9147057c8168ee0850f0fc967
Arthi Halder <arthihalder@gmail.com>
2026-07-15 23:59:06 +0600

    final commit 2

---
+++ README.md
@@ -0,0 +1,27 @@
+# SPC (Space Per Click)!
+have you even found youself zoning out and pressing space on your keyboard (i have found myself in this exact situation a lot) well dont worry now you can see how many time you clicked space for no reason!
+
+## how to use it
+just press space. its not that hard to figure out -_-
+if u want to forget how many clicks you took away from your space keys life you can press the button in bottom!
+
+## features
+* really clean and dark mode ui
+* round circle to show you click!
+* achivements are there too! u gotta figure it out yourself :/
+* oh and it can remember after u close the browser >:)
+
+
+## Screenshot of the page!
+yeah thats the only ss.
+![alt text](pagess.png)
+
+## how i made
+It is made with html, css and javascript. 
+
+copied my own code from different my own projects! 
+
+no but really this is one of the first project i built without the help of ai.
+
+
+## well hope you like it!
--- main.js
+++ main.js
@@ -11,7 +11,9 @@ document.addEventListener("keydown", function (e) {
     counter.textContent = count;
     localStorage.setItem("count",count);
     console.log(count);
-    clicker.style.backgroundColor = "green"
+    clicker.style.backgroundColor = "#39f772"
+    clicker.style.borderColor = "#39f772"
+    clicker.style.boxShadow = "0 0 20px #39f772";
     clicker.style.transform = "scale(1.2)";
   }
 });
@@ -20,10 +22,30 @@ document.addEventListener("keyup", function (e) {
   if (e.code === "Space") {
     e.preventDefault();
     clicker.style.backgroundColor = "#292929";
+        clicker.style.borderColor = "white"
+    clicker.style.boxShadow = "0 0 20px white";
     clicker.style.transform = "scale(1)";
+
+    if ( count === 10)
+        alert("alright, bet!");
+    if ( count === 50)
+        alert("50/50 you say?");
+    if ( count === 100)
+        alert("Thats a century!");
+    if ( count === 500)
+        alert("so you wanna get you work dont or wanna rest a bit?");
+    if ( count === 999)
+        alert("you arent use auto clicker ar you?");
+    if ( count === 5000)
+        alert("why? why are you doing this with your life?");
+    if ( count === 10000)
+        alert("You need to see a doctor :(");
+
   }
+
 });
 reset.addEventListener("click",() => {
     count = 0;
     counter.textContent = count;
+    localStorage.removeItem("count");
 })
